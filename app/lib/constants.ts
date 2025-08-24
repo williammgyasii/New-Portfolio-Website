edf915c45c3204ce4a430acad6ee7f6854d5b025
William Gyasi <gyasi.wk@gmail.com>
2025-08-23 22:07:40 -0400

    Added Shsadcn

---
+++ app/lib/constants.ts
@@ -0,0 +1,84 @@
+/**
+ * Effects configuration for UI visuals.
+ */
+export type EffectsConfig = {
+  mask: {
+    cursor: boolean;
+    x: number;
+    y: number;
+    radius: number;
+  };
+  gradient: {
+    display: boolean;
+    opacity: number;
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+    tilt: number;
+    colorStart: string;
+    colorEnd: string;
+  };
+  dots: {
+    display: boolean;
+    opacity: number;
+    size: string;
+    color: string;
+  };
+  grid: {
+    display: boolean;
+    opacity: number;
+    color: string;
+    width: string;
+    height: string;
+  };
+  lines: {
+    display: boolean;
+    opacity: number;
+    color: string;
+    size: string;
+    thickness: number;
+    angle: number;
+  };
+};
+
+const effects: EffectsConfig = {
+  mask: {
+    cursor: false,
+    x: 50,
+    y: 0,
+    radius: 100,
+  },
+  gradient: {
+    display: false,
+    opacity: 100,
+    x: 50,
+    y: 60,
+    width: 100,
+    height: 50,
+    tilt: 0,
+    colorStart: "accent-background-strong",
+    colorEnd: "page-background",
+  },
+  dots: {
+    display: true,
+    opacity: 40,
+    size: "2",
+    color: "brand-background-strong",
+  },
+  grid: {
+    display: false,
+    opacity: 100,
+    color: "neutral-alpha-medium",
+    width: "0.25rem",
+    height: "0.25rem",
+  },
+  lines: {
+    display: false,
+    opacity: 100,
+    color: "neutral-alpha-weak",
+    size: "16",
+    thickness: 1,
+    angle: 45,
+  },
+};
---
+++ app/lib/content.ts
@@ -0,0 +1,13 @@
+import { Person } from "../types/content.types";
+
+const person: Person = {
+  firstName: "William",
+  lastName: "Gyasi",
+  name: `William Gyasi`,
+  role: "Full Stack Developer",
+  avatar: "/images/avatar.jpeg",
+  email: "williammgyasii@gmail.com",
+  languages: ["English"], // optional: Leave the array empty if you don't want to display languages
+};
+
+export { person };
